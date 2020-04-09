2d91e492e7c538a514cbd3b2a91885493c04ccad
TheNooBest <mityankin@gmail.com>
2020-04-09 17:21:16 +0300

    Auto update messages with AJAX

--- static/js/main.js
+++ static/js/main.js
@@ -3,22 +3,39 @@ var paths = urlpath.split('/');
 var board_prefix = paths[1];
 var thread_id = paths[2];
 
-var update_posts = function() {
-    $.ajax({
-        url: '/api/board/' + board_prefix + '/thread/' + thread_id,
-        method: 'POST',
-        contentType: 'application/json;charset=UTF-8',
-        data: JSON.stringify({
-            'message': $('#message').val()
-        }, null, '\t'),
-    }).done(function(result) {
+var update_posts = function(message) {
+    var ajax;
+
+    if (message == '') {
+        ajax = $.ajax({
+            url: '/api/board/' + board_prefix + '/thread/' + thread_id,
+            method: 'GET'
+        });
+    }
+    else {
+        ajax = $.ajax({
+            url: '/api/board/' + board_prefix + '/thread/' + thread_id,
+            method: 'POST',
+            contentType: 'application/json;charset=UTF-8',
+            data: JSON.stringify({
+                'message': message
+            }, null, '\t')
+        });
+    }
+
+    ajax.done(function(result) {
         var postsString = '';
         for (var post in result) {
             postsString += '<br><h5><p>' + result[post].datetime + ': ' + result[post].message + '</p></h5>'
         }
         $('#posts').html(postsString);
     });
-    $('#message').val('');
 }
 
-$('#asd').click(update_posts)
+$('#asd').click(function() {
+    var message = $('#message').val();
+    update_posts(message);
+    $('#message').val('');
+})
+
+setInterval(update_posts, 3000, '')
