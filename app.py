b6150e98217c708b67231a0a7fa1d8ccaf2f33c6
TheNooBest <mityankin@gmail.com>
2020-04-24 00:35:26 +0300

    Api routes
    - Added Blueprint for api
    - Api routes moved to different file

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ db = SQLAlchemy(app)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+# routes
+from api import api
+app.register_blueprint(api, url_prefix='/api')
 from views import *
 
 @login_manager.user_loader
